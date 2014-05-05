2452fad248288cb90dc4f1c3728c302871298f5f
Glenn Downing <downing@cs.utexas.edu>
2014-05-04 23:27:42 -0700

    another commit

---
+++ exercises/Exercise1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# ----------
+# IsPrime.py
+# ----------
+
+from math import sqrt
+
+def is_prime (n) :
+    assert(n > 0)
+    if (n < 2) or ((n % 2) == 0) :
+        return False
+    for i in range(3, int(sqrt(n))) :
+        if (n % i) == 0:
+            return False
+    return True
+
+print("Exercise1.py")
+
+assert(not is_prime( 1));
+assert(    is_prime( 3));
+assert(not is_prime( 4));
+assert(    is_prime( 5));
+
+print("Done.")
+
+"""
+% coverage run --branch Exercise1.py
+Exercise1.py
+Done.
+
+% coverage report
+Name        Stmts   Miss Branch BrMiss  Cover
+---------------------------------------------
+Exercise1      16      2      6      3    77%
+"""
---
+++ quizzes/Quiz1.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+"""
+OOPL JPL: Quiz #1
+"""
+
+""" ----------------------------------------------------------------------
+1. What does the following program do?
+
+type(False): <class 'bool'>
+type(bool): <class 'type'>
+type(NameError()): <class 'NameError'>
+type(NameError): <class 'type'>
+type(type): <class 'type'>
+
+g1
+f1
+f2
+g2
+else
+finally
+g3
+
+g1
+f1
+NameError
+finally
+g3
+
+g1
+f1
+finally
+TypeError
+"""
+
+print("type(False):",       type(False))
+print("type(bool):",        type(bool))
+print("type(NameError()):", type(NameError()))
+print("type(NameError):",   type(NameError))
+print("type(type):",        type(type))
+print()
+
+def f (n) :
+    print("f1")
+    if (n % 3) == 1 :
+        raise NameError()
+    elif (n % 3) == 2 :
+        raise TypeError()
+    print("f2")
+
+def g (n) :
+    try :
+        try :
+            print("g1")
+            f(n)
+            print("g2")
+        except NameError :
+            print("NameError")
+        else :
+            print("else")
+        finally :
+            print("finally")
+        print("g3")
+    except TypeError :
+        print("TypeError")
+
+g(0)
+print()
+
+g(1)
+print()
+
+g(2)
