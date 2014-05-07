ad41edbeb1ea51616989333091e2b3a115e6254c
Glenn Downing <downing@cs.utexas.edu>
2014-05-07 11:53:09 -0700

    another commit

--- examples/FileInputOutput.py
+++ examples/FileInputOutput.py
@@ -21,3 +21,52 @@ f = open(sys.argv[0])
 for u in f :
     for v in u :
         print(v, " ", end = "")
+
+"""
+#!/usr/bin/env python3
+
+# ------------------
+# FileInputOutput.py
+# ------------------
+
+"""
+% FileInputOutput.py
+"""
+
+import sys
+
+f = open(sys.argv[0])
+
+for u in f :
+    for v in u :
+        print(v, end = "")
+
+f = open(sys.argv[0])
+
+for u in f :
+    for v in u :
+        print(v, " ", end = "")
+#  !  /  u  s  r  /  b  i  n  /  e  n  v     p  y  t  h  o  n  3
+
+  #     -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+  #     F  i  l  e  I  n  p  u  t  O  u  t  p  u  t  .  p  y
+  #     -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+
+  "  "  "
+  %     F  i  l  e  I  n  p  u  t  O  u  t  p  u  t  .  p  y
+  "  "  "
+
+  i  m  p  o  r  t     s  y  s
+
+  f     =     o  p  e  n  (  s  y  s  .  a  r  g  v  [  0  ]  )
+
+  f  o  r     u     i  n     f     :
+              f  o  r     v     i  n     u     :
+                          p  r  i  n  t  (  v  ,     e  n  d     =     "  "  )
+
+  f     =     o  p  e  n  (  s  y  s  .  a  r  g  v  [  0  ]  )
+
+  f  o  r     u     i  n     f     :
+              f  o  r     v     i  n     u     :
+                          p  r  i  n  t  (  v  ,     "     "  ,     e  n  d     =     "  "  )
+"""
