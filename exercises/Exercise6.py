a23b79f89f772b07297d5ba009a229c0a03d99fd
Glenn Downing <downing@cs.utexas.edu>
2014-05-08 09:03:47 -0700

    another commit

---
+++ exercises/Exercise6.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# ------------
+# Exercise6.py
+# ------------
+
+print("Exercise6.py")
+
+"""
+Design, test, and implement the following methods of a queue:
+
+back
+empty
+front
+pop
+push
+size
+"""
+
+print("Done.")
