9ef62530c04dd052c0f30435fa71a67e21d52189
Glenn Downing <downing@cs.utexas.edu>
2014-05-09 08:52:54 -0700

    another commit

--- quizzes/Quiz4.py
+++ quizzes/Quiz4.py
@@ -10,6 +10,21 @@ OOPL JPL: Quiz #4
 
 import re
 
+print([2, 3, 4, 5, 6][-4 : -1 :  2 ])
+print([2, 3, 4, 5, 6][-2 : -5 : -2 ])
+print()
+
+a = [[2], [3], [4]]
+for v in a :
+    v += (5,)
+print(a)
+
+a = [(2,), (3,), (4,)]
+for v in a :
+    v += (5,)
+print(a)
+print()
+
 s = "b xaby\nzaab 123"
 
 m = re.search("(a*)b([^a]*)(a*)b", s) # * is zero or more
