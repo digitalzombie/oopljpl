c37678914d2e9a302af870459d8570488ffe7e4b
Glenn Downing <downing@cs.utexas.edu>
2014-05-09 09:31:37 -0700

    another commit

--- projects/Collatz.py
+++ projects/Collatz.py
@@ -5,10 +5,16 @@
 % diff Collatz.tmp Collatz.out
 """
 
-import sys
+from sys import stdin, stdout
 
 def collatz_solve (r, w) :
+    s = r.readline()
+    if s == "" :
+        return False
+    l = s.split()
+    a[0] = int(l[0])
+    a[1] = int(l[1])
     w.write("1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
 
 if __name__ == '__main__' :
-    collatz_solve(sys.stdin, sys.stdout)
+    collatz_solve(stdin, stdout)
--- projects/TestCollatz.py
+++ projects/TestCollatz.py
@@ -4,16 +4,16 @@
 % TestCollatz.py >& TestCollatz.out
 """
 
-import io
-import unittest
+from io       import StringIO
+from unittest import TestCase
 
 from Collatz import collatz_solve
 
-class TestCollatz (unittest.TestCase) :
+class TestCollatz (TestCase) :
     def test_solve (self) :
-        r = io.StringIO("1 10\n100 200\n201 210\n900 1000\n")
-        w = io.StringIO()
+        r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
+        w = StringIO()
         collatz_solve(r, w)
         self.assertTrue(w.getvalue() == "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
 
-unittest.main()
+main()
