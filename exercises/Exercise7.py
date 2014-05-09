ea0a2438cba1cb94eb281a165de71a6ad14318b6
Glenn Downing <downing@cs.utexas.edu>
2014-05-08 23:33:45 -0700

    another commit

---
+++ exercises/Exercise7.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# ------------
+# Exercise7.py
+# ------------
+
+print("Exercise7.py")
+
+"""
+Design, test, and implement the following functions:
+
+read
+eval
+print
+
+to solve this UVa problem:
+
+http://www.spoj.pl/problems/PROBTNPO/
+
+"""
+print("Done.")
